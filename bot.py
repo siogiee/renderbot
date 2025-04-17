4f98d870c2de338420e0e76e9d3deeda514d4507
siogiee <110286612+siogiee@users.noreply.github.com>
2025-04-17 09:47:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ async def laporan_bulanini(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Total pengeluaran bulan ini: {total_rp}")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+    app = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("hariini", laporan_hariini))
